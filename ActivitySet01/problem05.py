4066a2381123966ec0c2c76eb7dab5278e990ef7
GodhaRG <ruparashmi@gmail.com>
2022-05-12 21:32:35 +0530

    program 5

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -9,4 +9,11 @@ hrs = float(input("Enter hours? "))
 rte = float(input("Enter rate per hour? "))
 
 p = computepay(hrs, rte)
+if hrs>40:
+    reg=rte*hrs
+    otp=(hrs-40.0) *(rte*0.5)
+    p=reg+otp
+else:
+    p=hrs*r3te
+
 print("Pay", p)
