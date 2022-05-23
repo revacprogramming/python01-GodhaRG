9c850ec2b8c410f390a5300a2817ed8f6540c8f1
GodhaRG <ruparashmi@gmail.com>
2022-05-23 09:53:12 +0530

    program 9

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,2 +1,11 @@
 #Lists
-filename=" dataset/romeo.txt "
+filename= "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+x=fh.readlines()
+lst = list()
+for i in fh.readlines():
+    for j in i.split():
+        if  j not in lst:
+             lst.append(j)
+print(sorted(lst))
