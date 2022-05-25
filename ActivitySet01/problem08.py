a3b616798c7ae1fedb5859bc9fb4aa341fb6751f
GodhaRG <ruparashmi@gmail.com>
2022-05-25 15:46:02 +0530

    program 10

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -7,7 +7,7 @@ n=0
 a=[]
 result=0
 for line in fh:
-    if not line.startswitch("X-DSPAM-Confidence:"):
+    if not line.startswith("X-DSPAM-Confidence:"):
         continue
     x=line.split()
     a.append(float(x[1]))
--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,14 @@
 # Dictionaries
 
-filename = " dataset/mbox-short.txt "
+filename = "dataset/mbox-short.txt"
+fname="dataset/mbox-short.txt"
+c=open(fname,"r")
+n=0
+h=c.readlines()
+for i in h:
+    k=i.split()
+    if i.startswith("From:"):
+        print(k[1])
+        n+=1
+print("There were",n,"lines in the file with From as the first word")
+#print(f"there were {n} lins in the file with from as the first word")
