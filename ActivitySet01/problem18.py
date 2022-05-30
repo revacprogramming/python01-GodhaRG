ef457b65799631eba6630c0510862bb09e028e92
GodhaRG <ruparashmi@gmail.com>
2022-05-30 10:23:03 +0530

    program 18

--- ActivitySet01/problem18.py
+++ ActivitySet01/problem18.py
@@ -1,9 +1,9 @@
 a=int(input("enter the number"))
 b=int(input("enter the number"))
 c=int(input("enter the number"))
-if a>=b and a>=c:
+if a>b and a>c:
     print("largest",a)
-elif b>=a and b>=c:
+elif b>c:
     print("largst",b)
 else :
     print("largest",c)
